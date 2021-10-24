2641f2242e588ed0e6fc2f31c0077911a20cf185
Louis Shakya <louisshakya@gmail.com>
2021-10-24 18:14:39 -0500

    Added tableview, custom protocol in the model, displaying titles in the table

--- ios-app/ViewController.swift
+++ ios-app/ViewController.swift
@@ -7,17 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ModelDelegate {
+
+
+    @IBOutlet weak var tableView: UITableView!
     
     var model = Model()
+    var categories = [Categories]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // Set itself as the dataSource
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Set itself as the delegate of the model 
+        model.delegate = self
+        
         model.getCategories()
     }
+    
+    // MARK: - model delegate methods
+    
+    func categoriesFetched(_ categories: [Categories]) {
+        
+        // Set the returned categories to our categories property
+        self.categories = categories
+        
+        // Refresh the tableview
+        tableView.reloadData()
+    }
 
+    // MARK: - tableView Methods
+    
+    
+    // dataSource methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CATEGORYCELL_ID, for: indexPath)
+        
+        // Configure the cell with the data
+        // Get the title for the category
+        let title = self.categories[indexPath.row].strCategory
+        
+        cell.textLabel?.text = title
+        
+        // Return the cell
+        return cell
+    }
+    
+    // delegate methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 
 }
 
