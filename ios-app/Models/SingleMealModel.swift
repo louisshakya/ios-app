1b9c5d0602ddab1046a26db5191d578020634894
Louis Shakya <louisshakya@gmail.com>
2021-10-30 18:16:41 -0500

    added segue method to passs the selected single meal

--- ios-app/Models/SingleMealModel.swift
+++ ios-app/Models/SingleMealModel.swift
@@ -7,11 +7,22 @@
 
 import Foundation
 
+protocol SingleMealModelDelegate {
+    func singleMealFetched(_ singleMeal:[SingleMeal])
+}
+
+
 class SingleMealModel {
+    
+    var delegate:SingleMealModelDelegate?
     
     func getData(_ mealID: String) {
         
+        print("i am inside single meal model get data")
+        
         let singleMealURL = Constants.SINGLE_MEAL_API_URL + mealID
+        
+        print(singleMealURL)
         
         let url = URL(string: singleMealURL)
         
@@ -23,6 +34,7 @@ class SingleMealModel {
         
         let dataTask = session.dataTask(with: url!) { data, response, error in
             
+            print("creating data task")
             if error != nil && data == nil {
                 print("An error occured while fetching data")
             }
@@ -31,8 +43,16 @@ class SingleMealModel {
                 
                 let decoder = JSONDecoder()
                 let response = try decoder.decode(MealResponse.self, from: data!)
-                print("i am an error wooooo")
-                dump(response)
+                print("created response")
+                if response.meals != nil {
+                    
+                    DispatchQueue.main.async {
+                        
+                        self.delegate?.singleMealFetched(response.meals!)
+                        
+                    }
+                    
+                }
 
             }
             catch {
